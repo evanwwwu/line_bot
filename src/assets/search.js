d7b4d267bafd87259dc622961aac1914e4c315ee
evan <evanwu80@gmail.com>
2018-05-17 01:30:40 +0800

    add 打球天氣

--- src/assets/search.js
+++ src/assets/search.js
@@ -4,11 +4,17 @@ const
     axios = require("axios");
 
 module.exports.search_hot = search_hot;
+module.exports.check_ball = check_ball;
 
-
-function search_hot() {
+function search_hot(type) {
     return new Promise((resolve) => {
-        request("https://www.jkforum.net/forum-736-1.html", function (err, res, body) {
+        if (type == "18+") {
+            uri = "https://www.jkforum.net/forum-535-1.html";
+        }
+        else {
+            uri = "https://www.jkforum.net/type-736-1938.html";
+        }
+        request(uri, function (err, res, body) {
             try {
                 const $ = cheerio.load(body);
                 const hrefs = $("#waterfall li h3 a");
@@ -34,4 +40,47 @@ function search_hot() {
             }
         })
     })
+}
+
+function check_ball() {
+    axios.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=rdec-key-123-45678-011121314')
+        .then((data) => {
+            const text = '';
+            const taipei = data.records.location[5];
+            const pop = taipei.weatherElement[1].time[2].parameter; //降雨機率
+            const minT = taipei.weatherElement[2].time[2].parameter; //最低溫度
+            const maxT = taipei.weatherElement[4].time[2].parameter; //最低溫度
+            const mixT = (minT + maxT) / 2;
+            // const CI = taipei.weatherElement[3].time[2].parameter; //舒適度
+            if (pop < 20) {
+                text = "沒雨，";
+                CmixT();
+            }
+            if (pop > 20 && pop < 60) {
+                text = "沒事，下雨機率不大，";
+                CmixT();
+            }
+            if (pop >= 60) {
+                text = "回家打...比較實際。";
+            }
+
+            return {
+                msg: text,
+                pop: pop,
+                temp: minT + "C ~ " + maxT + "C",
+            };
+
+            function CmixT() {
+                if (mixT < 16) {
+                    text += "不過有點冷但動一動就好，所以走吧，打球!!";
+                }
+                if (mixT > 16 && mixT < 25) {
+                    text += "天氣又超好不打球對得起自己嗎。";
+                }
+                if (mixT >= 25) {
+                    text += "沒事熱了一點而已，流點汗身體比較健康。"
+                }
+            }
+
+        })
 }
--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ const
     linebot = require("linebot"),
     express = require("express"),
     app = express();
-const {
-    search_hot
-} = require("./assets/search");
+const {search_hot, check_ball} = require("./assets/search");
+
+
 const bot = linebot({
     channelId: config.CHANNER_ID,
     channelSecret: config.CHANNEL_SECRET,
@@ -21,9 +21,12 @@ app.post("/", linebotParser);
 bot.on('message', function (event) {
     console.log(event);
     const { type, text } = event.message;
-   
-    if (/^hot/.test(text)) {
-        search_hot().then((data) => {
+    if (/hot/.test(text.toLowerCase())) {
+        let type = "";
+        if (/!{2,}/.test(test)){
+            type = "18+";
+        }
+        search_hot(type).then((data) => {
             console.log("data::",data);
             event.reply(data);
         })
@@ -33,8 +36,12 @@ bot.on('message', function (event) {
         //      console.log(data);
         //  });
     }
+    if (/打球$/.test(text)) {
+        let data = check_ball();
+        event.reply(data);
+    }
 });
-
+check_ball();
 app.set('port', (process.env.PORT || 5002));
 app.listen(app.get("port"), function () {
     console.log("running on port ", app.get("port"));
