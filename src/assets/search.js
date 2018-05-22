ede38ff717e4d4da1b258c2f485ef2561bb6991f
Evan.wu <evan.wu@friendo.com.tw>
2018-05-22 16:55:20 +0800

    update

--- src/assets/search.js
+++ src/assets/search.js
@@ -6,6 +6,7 @@ const
 module.exports.search_hot = search_hot;
 module.exports.check_ball = check_ball;
 module.exports.search_nba = search_nba;
+
 function search_hot(type) {
     return new Promise((resolve) => {
         if (type == "18+") {
@@ -51,46 +52,54 @@ function search_hot(type) {
 }
 
 function check_ball() {
-    axios.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=rdec-key-123-45678-011121314')
-        .then((data) => {
-            const text = '';
-            const taipei = data.records.location[5];
-            const pop = taipei.weatherElement[1].time[2].parameter; //降雨機率
-            const minT = taipei.weatherElement[2].time[2].parameter; //最低溫度
-            const maxT = taipei.weatherElement[4].time[2].parameter; //最低溫度
-            const mixT = (minT + maxT) / 2;
-            // const CI = taipei.weatherElement[3].time[2].parameter; //舒適度
-            if (pop < 20) {
-                text = "沒雨，";
-                CmixT();
-            }
-            if (pop > 20 && pop < 60) {
-                text = "沒事，下雨機率不大，";
-                CmixT();
-            }
-            if (pop >= 60) {
-                text = "回家打...比較實際。";
-            }
-
-            return {
-                msg: text,
-                pop: pop,
-                temp: minT + "C ~ " + maxT + "C",
-            };
-
-            function CmixT() {
-                if (mixT < 16) {
-                    text += "不過有點冷但動一動就好，所以走吧，打球!!";
+    return new Promise((resolve) => {
+        axios.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=rdec-key-123-45678-011121314')
+            .then((response) => {
+                try {
+                    // console.log(data);
+                    let data = response.data;
+                    var text = '';
+                    const taipei = data.records.location[5];
+                    const pop = taipei.weatherElement[1].time[1].parameter.parameterName; //降雨機率
+                    const minT = taipei.weatherElement[2].time[1].parameter.parameterName; //最低溫度
+                    const maxT = taipei.weatherElement[4].time[1].parameter.parameterName; //最低溫度
+                    const mixT = (minT + maxT) / 2;
+                    // const CI = taipei.weatherElement[3].time[2].parameter; //舒適度
+                    if (pop < 20) {
+                        text = "沒雨，";
+                        text += CmixT(mixT);
+                    }
+                    if (pop > 20 && pop < 60) {
+                        text = "沒事，下雨機率不大，";
+                        text += CmixT(mixT);
+                    }
+                    if (pop >= 60) {
+                        text = "回家打...比較實際。";
+                    }
+                    resolve({
+                        msg: text,
+                        pop: pop + "%",
+                        temp: minT + "C ~ " + maxT + "C",
+                    });
+                } catch (err) {
+                    console.error(err);
                 }
-                if (mixT > 16 && mixT < 25) {
-                    text += "天氣又超好不打球對得起自己嗎。";
-                }
-                if (mixT >= 25) {
-                    text += "沒事熱了一點而已，流點汗身體比較健康。"
-                }
-            }
+            });
 
-        })
+        function CmixT(temp) {
+            let m = "";
+            if (temp < 16) {
+                m = "不過有點冷但動一動就好，所以走吧，打球!!";
+            }
+            if (temp > 16 && temp < 25) {
+                m = "天氣又超好不打球對得起自己嗎。";
+            }
+            if (temp >= 25) {
+                m = "熱了一點而已，流點汗身體比較健康。"
+            }
+            return m;
+        }
+    });
 }
 
 function search_nba(search_date) {
@@ -116,7 +125,7 @@ function search_nba(search_date) {
                 let data = result.data;
                 let dates = data.payload.dates;
                 let game;
-                for (let x = 0; x < 7;x++){
+                for (let x = 0; x < 7; x++) {
                     if (dates[x].utcMillis > new Date().getTime()) {
                         game = dates[x].games[0];
                         break;
@@ -128,11 +137,11 @@ function search_nba(search_date) {
                 }
                 let play_date = new Date(parseInt(game.profile.utcMillis));
                 let date_text = play_date.getFullYear() + "-" + (play_date.getMonth() + 1) + "-" + play_date.getDate();
-                    date_text += "  " + addZero(play_date.getHours()) + ":" + addZero(play_date.getMinutes());
+                date_text += "  " + addZero(play_date.getHours()) + ":" + addZero(play_date.getMinutes());
                 tmp = "日期: " + date_text + "\n" + game.profile.arenaLocation + " " + game.profile.arenaName + "\n" + teams.home + "：" + teams.away;
                 msg.push(tmp);
                 resolve(msg);
-            });        
+            });
         } else {
             axios.get(`http://tw.global.nba.com/stats2/scores/daily.json`, {
                 params: {
@@ -157,7 +166,7 @@ function search_nba(search_date) {
                             away: game.boxscore.awayScore
                         }
                         let play_date = new Date(parseInt(game.profile.utcMillis));
-                        let date_text = play_date.getFullYear() + "-" + (play_date.getMonth()+1) + "-" + play_date.getDate();
+                        let date_text = play_date.getFullYear() + "-" + (play_date.getMonth() + 1) + "-" + play_date.getDate();
                         if (game.boxscore.status != '2') {
                             date_text += "  " + addZero(play_date.getHours()) + ":" + addZero(play_date.getMinutes());
                         }
@@ -176,7 +185,7 @@ function search_nba(search_date) {
                 }
                 resolve(msg);
             });
-        }    
-       
+        }
+
     })
 }
