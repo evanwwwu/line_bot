de597106c692ee3d8d16f033d198b75b50d0895f
evanwwwu <evanwu80@gmail.com>
2019-06-19 19:35:25 +0800

    trx done

--- src/main.js
+++ src/main.js
@@ -35,23 +35,32 @@ bot.on('message', async function (event) {
         const base_id = group_id || user_id;
 
         if (type == 'text') {
-            if (/^[!！]/.test(text)) { 
-                var newStr = text.replace(/[!！]/g, '');
+            if (/^!起床/.test(text)) {
+                event.reply('我起床了！');
+            }
+            if (/^[!！]trx/i.test(text)) { 
+                var newStr = text.replace(/[!！]trx/ig, '');
                 console.log(newStr)
                 if (/規則/.test(newStr)) {
-                    event.reply('this is test')
+                    const roleStr = '!trx {時間} {人數}\n範例：!trx 6/19(三) +1\n\n!trx 名單  =>可查看名單'
+                    event.reply(roleStr);
                 }
-                if (/^[+＋]\d+/.test(newStr)) {
+                if (/名單/.test(newStr)) {
+                    event.reply('https://docs.google.com/spreadsheets/d/1lKXoQrymYnH_i2S8e7MjMR9FErWkzVmJaQgjJcDWcg0/edit?usp=sharing');
+                }
+                if (/[+＋]\d+/.test(newStr)) {
                     const userData = await bot.getUserProfile(user_id);
                     let datas = newStr.replace(/[+＋]/,'').split(/\s+/);
                     let bodyData = new FormData();
                     bodyData.append('name', userData.displayName);
                     bodyData.append('date', datas[1]);
-                    bodyData.append('num', datas[0]);
+                    bodyData.append('num', datas[2]);
                     let res = await axios.post('https://script.google.com/macros/s/AKfycbwwC--_39aBvQJ5u8dPTlUCYtIeGL-QGh7Y-p21afQhD3HcLpNh/exec', bodyData, {
                         headers: bodyData.getHeaders(),
                     });
-                    console.log(userData.displayName, res.config, res.data);
+                    if (res.data) {
+                        event.reply('報名成功')
+                    }
                 }
             }
         }
