f8dc7dd55016c37bbd15b8afd1edb4f281a4ec97
evanwwwu <evanwu80@gmail.com>
2019-06-19 19:36:38 +0800

    build

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,9 @@
-const
-    config = require("../config"),
-    axios = require("axios"),
-    linebot = require("linebot"),
-    express = require("express"),
-    FormData = require('form-data'),
-    app = express();
+const config = require("../config"),
+      axios = require("axios"),
+      linebot = require("linebot"),
+      express = require("express"),
+      FormData = require('form-data'),
+      app = express();
 
 const version = "0.9";
 
@@ -20,7 +19,7 @@ app.post("/", linebotParser);
 
 app.get("/test", function (req, res) {
     console.log(__dirname);
-    res.sendFile(__dirname +"/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 bot.on('message', async function (event) {
@@ -38,11 +37,11 @@ bot.on('message', async function (event) {
             if (/^!起床/.test(text)) {
                 event.reply('我起床了！');
             }
-            if (/^[!！]trx/i.test(text)) { 
+            if (/^[!！]trx/i.test(text)) {
                 var newStr = text.replace(/[!！]trx/ig, '');
-                console.log(newStr)
+                console.log(newStr);
                 if (/規則/.test(newStr)) {
-                    const roleStr = '!trx {時間} {人數}\n範例：!trx 6/19(三) +1\n\n!trx 名單  =>可查看名單'
+                    const roleStr = '!trx {時間} {人數}\n範例：!trx 6/19(三) +1\n\n!trx 名單  =>可查看名單';
                     event.reply(roleStr);
                 }
                 if (/名單/.test(newStr)) {
@@ -50,27 +49,26 @@ bot.on('message', async function (event) {
                 }
                 if (/[+＋]\d+/.test(newStr)) {
                     const userData = await bot.getUserProfile(user_id);
-                    let datas = newStr.replace(/[+＋]/,'').split(/\s+/);
+                    let datas = newStr.replace(/[+＋]/, '').split(/\s+/);
                     let bodyData = new FormData();
                     bodyData.append('name', userData.displayName);
                     bodyData.append('date', datas[1]);
                     bodyData.append('num', datas[2]);
                     let res = await axios.post('https://script.google.com/macros/s/AKfycbwwC--_39aBvQJ5u8dPTlUCYtIeGL-QGh7Y-p21afQhD3HcLpNh/exec', bodyData, {
-                        headers: bodyData.getHeaders(),
+                        headers: bodyData.getHeaders()
                     });
                     if (res.data) {
-                        event.reply('報名成功')
+                        event.reply('報名成功');
                     }
                 }
             }
         }
-    }
-    catch (err) {
+    } catch (err) {
         console.error(err);
     }
 });
 
-app.set('port', (process.env.PORT || 5002));
+app.set('port', process.env.PORT || 5002);
 app.listen(app.get("port"), function () {
-    console.log("running on port %s version：%s ", app.get("port"),version);
-})
+    console.log("running on port %s version：%s ", app.get("port"), version);
+});
